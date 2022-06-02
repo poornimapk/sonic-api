c892c6a98a7dc0e0c2019753d94f91ac87083a3a
poornimapk <poornima.pk@gmail.com>
2022-06-02 12:54:12 -0700

    Added client side code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log("Working");
+document.querySelector('button').addEventListener('click', apiRequest);
+
+async function apiRequest(){
+    const characterName = document.querySelector('input').value;
+    try{
+        const response = await fetch(`https://sonic-api-100devs.herokuapp.com/api/${characterName}`);
+        const data = await response.json();
+
+        console.log(data);
+        document.querySelector('h2').innerText = data.powers;
+    }catch(error){
+        console.log(error);
+    }
+}
